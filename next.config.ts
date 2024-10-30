5b00a0767ad0613345489e04f484fa2997294916
Pascal Bourque <pascal@cosmos.moi>
2024-10-30 11:30:41 -0400

    ci: Create GitHub CI workflow (#1)
    * Create lint.yml
    
    * Prettier
    
    * Fixed NODE_VERSION variable references
    
    * npm ci --force
    
    * Added lint-format
    
    * Format current.json
    
    * Multi-locale build
    
    * Fixed env var; added NextJS build caching
    
    * Combine build outputs and store as artifact
    
    * Fixed artifact directory
    
    * Include the language directory in intermediate artifact
    
    * Packaging all files required for the WeeWX skin
    
    * Changed the packaged directory name
    
    * Renamed the archive

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  distDir: `out/${process.env.METEO_BUILD_LOCALE || 'en'}`
 };
 
 export default withNextIntl(nextConfig);
