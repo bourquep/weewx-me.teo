c67d0d69913eb664be45c114ca6166ffa8c33cd6
Pascal Bourque <pascal@cosmos.moi>
2024-10-31 16:39:18 -0400

    Added @semantic-release/changelog plugin

--- release.config.mjs
+++ release.config.mjs
@@ -6,6 +6,7 @@ const semanticReleaseConfig = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/github'
   ]
 };
