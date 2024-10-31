4e7aafe27ae00d720bf63dc95b65ef89110814d4
Pascal Bourque <pascal@cosmos.moi>
2024-10-31 09:33:18 -0400

    Tweaked the color palette

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,19 @@
 'use client';
+import { blueGrey, cyan, orange } from '@mui/material/colors';
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 let theme = createTheme({
   typography: {
     fontFamily: 'var(--font-roboto)'
+  },
+  palette: {
+    primary: {
+      main: cyan[400]
+    },
+    secondary: {
+      main: orange[400]
+    },
+    grey: blueGrey
   },
   colorSchemes: {
     light: true,
