74f656b369564a82bc73bc01e8712b1d5fd4804e
Pascal Bourque <pascal@cosmos.moi>
2024-11-08 20:41:17 -0500

    ci: Configured semantic-release to create a patch release for dependencies

--- release.config.mjs
+++ release.config.mjs
@@ -4,7 +4,12 @@
 const semanticReleaseConfig = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [{ type: 'chore', scope: 'deps', release: 'patch' }]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/exec',
